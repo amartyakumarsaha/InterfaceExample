c07ad5f6c1171d9c39086c22ece4edd1a4481f74
amartyakumarsaha <68885990+amartyakumarsaha@users.noreply.github.com>
2020-08-30 20:27:53 +0530

    Add files via upload

---
+++ InterfaceExample.java
@@ -0,0 +1,32 @@
+interface Sum{
+    void summation();   //Compiler will Provide  " public abstract " automatically
+}
+interface Subtract{
+    public abstract void subtraction(); //Here We provided the " public abstract " 
+}
+class Operation implements Sum,Subtract{
+    int a,b;
+    Operation(int a,int b)
+    {
+        this.a=a;
+        this.b=b;
+    }
+    @Override
+    public void summation()
+    {
+        System.out.println(a+b);
+    }
+    @Override
+    public void subtraction()
+    {
+        System.out.println(a-b);
+    }
+}
+public class InterfaceExample {
+    public static void main(String args[])
+    {
+        Operation obj=new Operation(10,5);
+        obj.summation();
+        obj.subtraction();
+    }
+}
